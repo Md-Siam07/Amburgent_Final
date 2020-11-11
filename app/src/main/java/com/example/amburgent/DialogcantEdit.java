21cb78bdc3e76d38d0f237676d27cbd6c808f445
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-11-11 13:00:50 +0600

    changed

---
+++ app/src/main/java/com/example/amburgent/DialogcantEdit.java
@@ -0,0 +1,24 @@
+package com.example.amburgent;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class DialogcantEdit extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view  = inflater.inflate(R.layout.activity_dialogcant_edit,null);
+
+        builder.setView(view);
+        return builder.create();
+    }
+
+}
